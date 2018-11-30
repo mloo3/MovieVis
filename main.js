e48cbbde97ee836a3a34284b11c62c16161413c5
mloo3 <slayer71432@gmail.com>
2018-11-29 20:31:22 -0500

    added word cloud

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+var fill = d3.scale.category20();
 window.onload = start;
 
 function start() {
     d3.csv('movies.csv', function(d) {
         keys = Object.keys(d[0])
+        genreFrequency = {};
         d.forEach(function(d) {
             d[keys[2]] = +d[keys[2]];
             d[keys[3]] = +d[keys[3]];
@@ -21,12 +23,41 @@ function start() {
             d[keys[25]] = +d[keys[25]];
             d[keys[26]] = +d[keys[26]];
             d[keys[27]] = +d[keys[27]];
-        })
+            genres = d[keys[9]].split('|')
+            genres.forEach(function(g) {
+               genreFrequency[g] = (genreFrequency[g] || 0) + 1;
+            });
+        });
+        console.log(Object.keys(genreFrequency).length);
+        d3.layout.cloud().size([300,300])
+            .words(Object.keys(genreFrequency).map(function(key, index) {
+                return {text: key, size: genreFrequency[key]};
+            }))
+            .padding(5)
+            .rotate(function() { return ~~(Math.random() * 2) * 90; })
+            .font("Impact")
+            .fontSize(function(d) {
+                return d.size;})
+            .on("end", draw)
+            .start();
+    });
+}
 
-        // console.log(keys);
-        // d[0].keys[2] = +d[0].keys[2];
-        console.log(d[0]);
-        console.log(keys[2]);
-
+function draw(words) {
+d3.select("#cloud").append("svg")
+    .attr("width", 300)
+    .attr("height", 300)
+  .append("g")
+    .attr("transform", "translate(150,150)")
+  .selectAll("text")
+    .data(words)
+  .enter().append("text")
+    .style("font-size", function(d) { return d.size + "px"; })
+    .style("font-family", "Impact")
+    .style("fill", function(d, i) { return fill(i); })
+    .attr("text-anchor", "middle")
+    .attr("transform", function(d) {
+      return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
     })
+    .text(function(d) { return d.text; });
 }
